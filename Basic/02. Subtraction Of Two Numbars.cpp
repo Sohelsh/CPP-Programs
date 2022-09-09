d57a21da7bb7c8fee1115915917a4cb82ec30591
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-09 20:29:16 +0530

    Added New File

---
+++ Basic/02. Subtraction Of Two Numbars.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+    int Num1 = 0,Num2 = 0,Sub = 0;
+
+    cout<<endl<<"Enter Two Numbers = ";
+    cin>>Num1>>Num2;
+
+    Sub = Num1 - Num2;
+
+    cout<<endl<<"Subtraction Of Two Numbers = "<<Sub;
+
+    getch();
+    return 0;
+}
