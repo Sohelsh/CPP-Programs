8398e88f286cea757e2c72387e5938550219de23
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-09 20:25:01 +0530

    Added CPP Programs

---
+++ Basic/01.Additon Of Two Numbers.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 0,No2 = 0;
+
+    cout<<endl<<"Enter Two Nubers = ";
+    cin>>No1>>No2;
+
+   // Sum = No1 + No2;
+
+    cout<<endl<<"Sum Of Two Numbers = "<<No1+No2;
+
+    getch();
+    return 0;
+}
