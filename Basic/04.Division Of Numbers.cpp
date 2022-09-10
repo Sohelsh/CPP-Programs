e13397f0f3e4b56ac8b7d8de1cfac4a347384a99
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-10 21:16:00 +0530

    Basic Program

---
+++ Basic/04.Division Of Numbers.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 0,No2 = 0,Div = 0;
+
+    cout<<endl<<" Enter Two Numbers = ";
+    cin>>No1>>No2;
+
+    Div = No1 / No2;
+
+    cout<<endl<<"\a Division Of Two Numbers = "<<Div;
+
+    getch();
+    return 0;
+}
