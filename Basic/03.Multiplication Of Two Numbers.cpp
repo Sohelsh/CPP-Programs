93b8e56ac31ad62c7ef4ffb34fc8b2cd1262dbc9
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-10 21:14:24 +0530

    Basic Program

---
+++ Basic/03.Multiplication Of Two Numbers.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+    int N1 = 0,N2 = 0,Mul = 0;
+
+    cout<<endl<<"Enter Two Numbers = ";
+    cin>>N1>>N2;
+
+    Mul = N1 * N2;
+
+    cout<<endl<<"Multiplication Of Two Numbers = "<<Mul;
+
+    getch();
+    return 0;
+}
